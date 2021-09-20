cc1ddccde907da1c01bb411b0b4214ec1e4e2b3f
Vitalii Rudenko <rvsrudik@gmail.com>
2021-09-20 21:46:49 +0300

    implement orders list with vuetify table

--- src/api/services.js
+++ src/api/services.js
@@ -1,3 +1,6 @@
+import { ordersGenerator } from '@/utils/generateOrders.js';
+const orders = ordersGenerator(98);
+
 export function loginRequest(login, password) {
   const isLoginSuccess = login === 'admin' && password === '!@12345';
   
@@ -10,45 +13,22 @@ export function loginRequest(login, password) {
   });  
 }
 
+export function getOrders(page = 1, itemsPerPage = 10) {
+  const startFrom = (page - 1) * itemsPerPage;
 
-const orders = [
-  {
-    id: 1,
-    name: 'Order 1',
-    description: 'Description 1',
-  },
-  {
-    id: 2,
-    name: 'Order 2',
-    description: 'Description 2',
-  },
-  {
-    id: 3,
-    name: 'Order 3',
-    description: 'Description 3',
-  },
-  {
-    id: 4,
-    name: 'Order 4',
-    description: 'Description 4',
-  },
-  {
-    id: 5,
-    name: 'Order 5',
-    description: 'Description 5',
-  },
-]
-
-export function getOrders() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ status: 'ok', payload: orders })
+      resolve({ 
+        status: 'ok', 
+        payload: {
+          result: orders.slice(startFrom, startFrom + itemsPerPage),
+          total: orders.length,
+        }})
     }, 500);
   });  
 }
 
 export function getOrderById(id) {
-  console.log('id', id);
   const order = orders.find(o => parseInt(o.id) === parseInt(id));
   return new Promise((resolve, reject) => {
     setTimeout(() => {
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="orders">
     <h1>Orders</h1>
-    
-    <v-divider class="my-5" />
-    <v-fade-transition>
-      <v-progress-circular v-if="loading.orders" indeterminate class="orders__loader" />
-      <v-row v-else>
-        <v-col
-          v-for="order in orders"
-          :key="order.id"
-          cols="12"
-          sm="4">
-          <v-card @click="goToOrder(order.id)">
-            <v-card-title>{{ order.name }}</v-card-title>
-            <v-card-text>{{ order.description }}</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-fade-transition>
+
+    <v-data-table
+      :headers="headers"
+      :items="orders"
+      :items-per-page="pagination.itemsPerPage"
+      :page="pagination.page"
+      :server-items-length="totalOrders"
+      :loading="loading.orders"
+      disable-filtering
+      @pagination="onPaginationChanged"
+    />
   </div>
 </template>
 
@@ -28,9 +22,19 @@ export default {
   name: 'Orders',
   data() {
     return {
+      headers: [
+        { text: 'Id', value: 'id' },
+        { text: 'Name', value: 'name' },
+        { text: 'Description', value: 'description' },
+      ],
       orders: [],
+      totalOrders: null,
       loading: {
         orders: false,
+      },
+      pagination: {
+        page: 1,
+        itemsPerPage: 10,
       }
     };
   },
@@ -42,8 +46,9 @@ export default {
       this.loading.orders = true;
 
       try {
-        const response = await getOrders();
-        this.orders = response.payload;
+        const response = await getOrders(this.pagination.page, this.pagination.itemsPerPage);
+        this.orders = response.payload.result;
+        this.totalOrders = response.payload.total
       } catch (err) {
         this.$notify.error(err.error)
       } finally {
@@ -52,6 +57,12 @@ export default {
     },
     goToOrder(id) {
       this.$router.push({ name: 'OrderItem', params: { id } })
+    },
+    onPaginationChanged(pagination) {
+      this.pagination.page = pagination.page;
+      this.pagination.itemsPerPage = pagination.itemsPerPage;
+
+      this.loadOrders();
     }
   }
 }
